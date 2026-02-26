ead7c75a85689f984645e054d00a5884a4e25fa3
Klyde Bairulla <klydefb@gmail.com>
2026-02-26 22:02:52 +0800

    Add project files.

---
+++ Approximately.Web/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.AspNetCore.Components.Web;
+global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+global using Approximately.Web;
+
+global using Blazored.Toast;
